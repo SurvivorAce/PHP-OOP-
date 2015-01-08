f06e849a40593e42ea65acdc694b89711c8ddbea
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-01-08 00:58:39 -0800

    no message

---
+++ Lecture2.php
@@ -0,0 +1,61 @@
+<?php
+<!-- Example 1  : -->
+
+	class Person1{
+	public $firstName;
+	public $lastName;
+	public $ethnicity;
+
+		function __construct($title, $firstName, $lastName, $ethnicity) {
+			$this->firstName = $firstName;
+			$this->lastName = $lastName;
+			$this->ethnicity = $ethnicity;
+		}
+
+		function getName() {
+		return "{$this->firstName}" . 
+		"{$this->lastName}";
+		}
+
+	class Person2{
+	public $firstName;
+	public $lastName;
+	public $ethnicity;
+
+		function __construct($title, $firstName, $lastName, $ethnicity) {
+			$this->firstName = $firstName;
+			$this->lastName = $lastName;
+			$this->ethnicity = $ethnicity;
+		}
+
+		function getName() {
+		return "{$this->firstName}" . 
+		"{$this->lastName}";
+		}
+
+	class Person3{
+	public $firstName;
+	public $lastName;
+	public $ethnicity;
+
+		function __construct($title, $firstName, $lastName, $ethnicity) {
+			$this->firstName = $firstName;
+			$this->lastName = $lastName;
+			$this->ethnicity = $ethnicity;
+		}
+
+		function getName() {
+		return "{$this->firstName}" . 
+		"{$this->lastName}";
+		}
+
+<!-- Example 2  : -->
+
+$Blake = new Person1(“Blake”, “Dayman”, “Asian”);
+print “Person 1: {$Blake­>getName()}\n;
+$Ulises = new Person2(“Ulises”, “Gomez”, “Mexican”);
+print “Person 2: {$Ulises­>getName()}\n;
+$Dimitri = new Person3(“Dimitri”, “Nooch”, “Asian”);
+print “Person 3: {$Dimitri­>getName()}\n;
+
+?>
