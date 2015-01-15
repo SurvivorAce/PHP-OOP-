158a5cec2ced3a00721affc8a31dcbd4b7ecd4d7
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-01-15 11:57:08 -0800

    Fixed errors for Lecture 2

--- Lecture2.php
+++ Lecture2.php
@@ -1,61 +1,61 @@
 <?php
-<!-- Example 1  : -->
+/* Example 1 */
 
 	class Person1{
 	public $firstName;
 	public $lastName;
 	public $ethnicity;
 
-		function __construct($title, $firstName, $lastName, $ethnicity) {
+		function __construct($firstName, $lastName, $ethnicity) {
 			$this->firstName = $firstName;
 			$this->lastName = $lastName;
 			$this->ethnicity = $ethnicity;
 		}
 
 		function getName() {
-		return "{$this->firstName}" . 
-		"{$this->lastName}";
+		return $this->firstName . " " . $this->lastName;
 		}
-
+}
 	class Person2{
 	public $firstName;
 	public $lastName;
 	public $ethnicity;
 
-		function __construct($title, $firstName, $lastName, $ethnicity) {
+		function __construct($firstName, $lastName, $ethnicity) {
 			$this->firstName = $firstName;
 			$this->lastName = $lastName;
 			$this->ethnicity = $ethnicity;
 		}
 
 		function getName() {
-		return "{$this->firstName}" . 
-		"{$this->lastName}";
+		return $this->firstName . " " . $this->lastName;
 		}
-
+}
 	class Person3{
 	public $firstName;
 	public $lastName;
 	public $ethnicity;
 
-		function __construct($title, $firstName, $lastName, $ethnicity) {
+		function __construct($firstName, $lastName, $ethnicity) {
 			$this->firstName = $firstName;
 			$this->lastName = $lastName;
 			$this->ethnicity = $ethnicity;
 		}
 
 		function getName() {
-		return "{$this->firstName}" . 
-		"{$this->lastName}";
+		return $this->firstName . " " . $this->lastName;
 		}
+}
+/* Example 2 */
 
-<!-- Example 2  : -->
-
-$Blake = new Person1(“Blake”, “Dayman”, “Asian”);
-print “Person 1: {$Blake­>getName()}\n;
-$Ulises = new Person2(“Ulises”, “Gomez”, “Mexican”);
-print “Person 2: {$Ulises­>getName()}\n;
-$Dimitri = new Person3(“Dimitri”, “Nooch”, “Asian”);
-print “Person 3: {$Dimitri­>getName()}\n;
+$Blake = new Person1("Blake", "Dayman", "Asian");
+print "Person 1: " . $Blake->getName();
+echo "<br>";
+$Ulises = new Person2("Ulises", "Gomez", "Mexican");
+print "Person 2: " . $Ulises->getName();
+echo "<br>";
+$Dimitri = new Person3("Dimitri", "Nooch", "Asian");
+print "Person 3: " . $Dimitri->getName();
+echo "<br>";
 
 ?>
